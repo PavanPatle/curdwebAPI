e0339d90245cdf113818ec4d7716fa70fead88cf
unknown <pavankumar.patle@QMC046-PavanP.qmcpl.com>
2016-04-12 13:24:41 +0530

    testcommit

---
+++ CRUDwithWebAPIandAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDwithWebAPIandAngular.Startup))]
+namespace CRUDwithWebAPIandAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
